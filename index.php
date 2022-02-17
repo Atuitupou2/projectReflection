2377618cc0e1c8ba050bb873286399896606ba82
Atuitupou2 <80292030+Atuitupou2@users.noreply.github.com>
2022-02-17 11:52:10 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+ <!-- Bootstrap First Before others-->
+ <?php include 'styleSheets.php';?>
+
+ 
+<!-- Search bar and navigation links go here -->
+<div class="container-fluid">  
+    <div class="row">
+<!-- This is the blank space into thirds -->
+        <div class="col-3"> </div>  
+<!-- This is the content space into thirds -->
+        <?php include 'content.php';?>
+<!-- This is the blank space2 into thirds  -->
+        <div class="col">
+ 
+        </div>
+
+    </div>
+</div>
+
+</head>
+
+<body>
+
+</body>
+
+</html>
+
+
+
+
+
